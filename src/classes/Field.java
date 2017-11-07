de9cd663661805d2683e3dc58ae23bf2af9c5c0f
Ivan Ke <21key47@mybhs.org>
2017-11-07 15:43:44 -0800

    Added field

---
+++ src/classes/Field.java
@@ -0,0 +1,7 @@
+package classes;
+
+public class Field {
+	public Object head1;
+	public Object head2;
+	public Object ball;
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,6 @@
 package main;
 
 public class Main {
-	public Object head1;
-	public Object head2;
-	public Object ball;
+	
 	
 }
