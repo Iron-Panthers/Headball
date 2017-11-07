b12fb06eddf3b59fce3f18b1af86c17022c79905
Ivan Ke <21key47@mybhs.org>
2017-11-06 16:36:38 -0800

    Initial commit: everything except .classpath & .class

---
+++ src/classes/Object.java
@@ -0,0 +1,14 @@
+package classes;
+
+public class Object {
+	public double mass;
+	public double xVelocity;
+	public double yVelocity;
+	public double acceleration;
+	public double position;
+	public Object(double mass, double xVelocity, double yVelocity, double acceleration, double position) {
+		this.mass = mass;
+		this.xVelocity = xVelocity;
+		this.yVelocity = yVelocity;
+	}
+}
