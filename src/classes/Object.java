415a724cb97e10af2edf68deae9793d283aaa8d9
Ivan Ke <21key47@mybhs.org>
2017-11-07 16:17:25 -0800

    Updated object class and wall

--- src/classes/Object.java
+++ src/classes/Object.java
@@ -6,12 +6,16 @@ public abstract class Object {
 	double yVelocity;
 	double acceleration;
 	double position;
-	public Object(double mass, double xVelocity, double yVelocity, double acceleration, double position) {
+	double width;
+	double height;
+	public Object(double mass, double xVelocity, double yVelocity, double acceleration, double position, double width, 	double height) {
 		this.mass = mass;
 		this.xVelocity = xVelocity;
 		this.yVelocity = yVelocity;
 		this.acceleration = acceleration;
 		this.position = position;
+		this.width = width;
+		this.height = height;
 	}
 	public void move() {
 		
--- src/objects/Wall.java
+++ src/objects/Wall.java
@@ -1,5 +1,8 @@
 package objects;
 
-public class Wall {
-
+public class Wall extends Object {
+	public Object wall;
+	public Wall() {
+		wall = new Object(Double.POSITIVE_INFINITY,0,0,0,0);
+	}
 }
