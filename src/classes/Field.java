fcaee94130c3a7a06ec7eef81c9627f657d0f7ac
Ivan Ke <21key47@mybhs.org>
2017-11-07 17:02:04 -0800

    Added all objects to field

--- src/classes/Field.java
+++ src/classes/Field.java
@@ -1,6 +1,7 @@
 package classes;
 
 import objects.Ball;
+import objects.Goal;
 import objects.Head;
 import objects.Wall;
 
@@ -11,9 +12,14 @@ public class Field {
 	public Head head1;
 	public Head head2;
 	public Ball ball;
+	public Goal goal1;
+	public Goal goal2;
 	public Field() {
-		this.wall = new Wall(0,0,0,0);
+		this.wall = new Wall(0,0,screenW,screenH);
 		this.head1 = new Head();
 		this.head2 = new Head();
+		this.ball = new Ball();
+		this.goal1 = new Goal();
+		this.goal2 = new Goal();
 	}
 }
