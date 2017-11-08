9b3ebbb87ca411b61a9f31e3a3a386f9d6696bce
LincolnFreedman <21freedmanl96@mybhs.org>
2017-11-07 21:22:19 -0800

    Auto stash before merge of "fieldBranch" and "origin/fieldBranch"
    it has fixed stuff

--- src/classes/Field.java
+++ src/classes/Field.java
@@ -6,16 +6,16 @@ import objects.Head;
 import objects.Wall;
 
 public class Field {
-	double screenW;
-	double screenH;
-	public Wall wall;
+	double screenW = Constants.ScreenW;
+	double screenH = Constants.ScreenH;
+	public Wall wall;o
 	public Head head1;
 	public Head head2;
 	public Ball ball;
 	public Goal goal1;
 	public Goal goal2;
 	public Field() {
-		this.wall = new Wall(0,0,screenW,screenH);
+		this.wall = new Wall(0,0,0,0);
 		this.head1 = new Head();
 		this.head2 = new Head();
 		this.ball = new Ball();
