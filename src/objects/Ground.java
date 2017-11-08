8b7abe2c26344c15a89258c787a04bfeace541e7
Ivan Ke <21key47@mybhs.org>
2017-11-07 16:51:13 -0800

    Deleted ground which is basically a wall

--- src/objects/Ground.java
+++
@@ -1,9 +0,0 @@
-package objects;
-
-import classes.Object;
-
-public class Ground extends Object {
-	public Ground(double x, double y, double w, double h) {
-		super(Double.POSITIVE_INFINITY, 0, 0, 0, 0, x, y, w, h);
-	}
-}
