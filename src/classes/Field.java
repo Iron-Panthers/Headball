e41d1cbd4b4a37b70923d7a15d68d5cc5c8c2762
Ivan Ke <21key47@mybhs.org>
2017-11-07 16:53:48 -0800

    Updated Field

--- src/classes/Field.java
+++ src/classes/Field.java
@@ -1,14 +1,19 @@
 package classes;
 
+import objects.Ball;
+import objects.Head;
 import objects.Wall;
 
 public class Field {
 	double screenW;
 	double screenH;
 	public Wall wall;
-	public Object head2;
-	public Object ball;
+	public Head head1;
+	public Head head2;
+	public Ball ball;
 	public Field() {
 		this.wall = new Wall(0,0,0,0);
+		this.head1 = new Head();
+		this.head2 = new Head();
 	}
 }
