109a1df8c13da3376d73b197f9f689e9902a5a8e
LincolnFreedman <21freedmanl96@mybhs.org>
2017-11-13 15:57:01 -0800

    movement

--- src/classes/Constants.java
+++ src/classes/Constants.java
@@ -1,6 +1,6 @@
 package classes;
 
 public class Constants {
-	public static final int screenW = 768;
-	public static final int screenH = 512;
+	public static final int ScreenW = 768;
+	public static final int ScreenH = 512;
 }
