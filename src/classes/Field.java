118203406bff86026fd25747826810eda35cdb60
Ivan Ke <21key47@mybhs.org>
2017-11-13 16:15:42 -0800

    Fixed field

--- src/classes/Field.java
+++ src/classes/Field.java
@@ -8,14 +8,14 @@ import objects.Wall;
 public class Field {
 	double screenW = Constants.ScreenW;
 	double screenH = Constants.ScreenH;
-	public Wall wall;o
+	public Wall wall;
 	public Head head1;
 	public Head head2;
 	public Ball ball;
 	public Goal goal1;
 	public Goal goal2;
 	public Field() {
-		this.wall = new Wall(0,0,0,0);
+		this.wall = new Wall(0,0,screenW,screenH);
 		this.head1 = new Head();
 		this.head2 = new Head();
 		this.ball = new Ball();
