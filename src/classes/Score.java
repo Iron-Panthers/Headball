33f2f5576e3422068e268d8473d2f22e87108310
Ivan Ke <21key47@mybhs.org>
2017-11-15 15:05:39 -0800

    Auto stash before merge of "fieldBranch" and "origin/fieldBranch"

--- src/classes/Score.java
+++ src/classes/Score.java
@@ -1,11 +1,16 @@
 package classes;
 
+import main.Main;
+import objects.Ball;
+
 public class Score {
 	int score;
 	public Score() {
 	}
-	public void add() {
-		score++;
+	public void scored() {
+		if (Main.field.ball.xPosition = 1 && Main.field.ball.yPosition = 1) {
+			score++;
+		}
 	}
 	public void initialize() {
 		score = 0;
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,10 @@
 package main;
 
+import classes.Field;
+
 public class Main {
-	
-	
+	public static Field field;
+	public static void main(String[]args) {
+		
+	}
 }
