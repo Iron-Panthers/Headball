5cd021b34586fe44a2530aebf8797c2f05bca392
Ivan Ke <21key47@mybhs.org>
2017-11-15 16:41:13 -0800

    Spaces

--- src/objects/Player.java
+++ src/objects/Player.java
@@ -8,13 +8,13 @@ public class Player extends Object {
 		feet = new Feet(0,0,x,y);
 	}
 	public static void playerMoveRight(){
-		if(xVelocity<1) {
+		if(xVelocity < 1) {
 			applyforce(0.33,0);//this is how I think youll do it, with a parameter for amount of force being applied x and y, I might be wrong. the equation assumes i want speed always to be 1 for player movement
 
 		}
 	}
 	public static void playerMoveLeft() {
-		if(xVelocity>-1) {
+		if(xVelocity > -1) {
 			applyforce(-0.33,0);
 		}
 	}
