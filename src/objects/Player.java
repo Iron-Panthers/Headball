647bb46a122222c5030ab0a9c3a66e4aa2cf1b95
Ivan Ke <21key47@mybhs.org>
2017-11-15 16:23:43 -0800

    Fixed stuff

--- src/objects/Player.java
+++ src/objects/Player.java
@@ -7,18 +7,15 @@ public class Player extends Object {
 		head = new Head(0, 0, x, y);
 		feet = new Feet(0,0,x,y);
 	}
-	public void playerMoveRight(){
+	public static void playerMoveRight(){
 		if(xVelocity<1) {
 			applyforce(0.33,0);//this is how I think youll do it, with a parameter for amount of force being applied x and y, I might be wrong. the equation assumes i want speed always to be 1 for player movement
 
 		}
 	}
-	public void playerMoveLeft() {
+	public static void playerMoveLeft() {
 		if(xVelocity>-1) {
 			applyforce(-0.33,0);
 		}
-	}
-	public static void playerMoveLeft() {
-		applyforce(0,1 - yVelocity * mass);
 	}
 }
